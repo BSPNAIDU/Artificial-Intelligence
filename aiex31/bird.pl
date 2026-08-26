5ef88f6f258445a2eaf87dc9cbf8ec8a9c023c5d
Bhavani Shanker <bhavanishankerbhale1@gmail.com>
2026-08-26 08:27:11 +0530

    Add AI lab Prolog programs

---
+++ aiex31/bird.pl
@@ -0,0 +1,13 @@
+% Bird Database
+
+bird(ostrich, cannot_fly).
+bird(penguin, cannot_fly).
+bird(kiwi, cannot_fly).
+bird(eagle, can_fly).
+bird(pigeon, can_fly).
+bird(sparrow, can_fly).
+
+% Rule to check whether a bird can fly
+
+can_fly(Bird) :-
+    bird(Bird, can_fly).
